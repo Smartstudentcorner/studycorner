0394d14b5e0de0eee7339aa7423730e33c0a6d39
Smartstudentcorner <Smartstudentcorner@gmail.com>
2025-06-29 22:31:05 +0530

    Add files via upload

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div className="text-center">
+      <h1>Contact Us</h1>
+      <p>Reach out to us anytime. We’d love to hear from you!</p>
+    </div>
+  );
+};
+
+export default Contact;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,1 +1,12 @@
+import React from "react";
 
+const Home = () => {
+  return (
+    <div className="text-center">
+      <h1>Welcome to My React App</h1>
+      <p>This is the home page built with Bootstrap 5 and React Router.</p>
+    </div>
+  );
+};
+
+export default Home;
