152595fc82aad5160362a0c979c0014690ea05e1
Smartstudentcorner <Smartstudentcorner@gmail.com>
2025-06-29 22:40:45 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import { APP_NAME } from "./config";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import About from "./pages/About";
