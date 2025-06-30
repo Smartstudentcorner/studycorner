433007457598a3db0657716add4401b61bbe0377
Smartstudentcorner <Smartstudentcorner@gmail.com>
2025-06-30 23:03:06 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
 function App() {
-  const [message, setMessage] = useState('');
-
-  useEffect(() => {
-    fetch('/api/message')
-      .then(res => res.json())
-      .then(data => setMessage(data.message));
-  }, []);
-
   return (
-    <div className="container text-center mt-5">
-      <h1>React + Node Web App</h1>
-      <p className="lead text-secondary">{message}</p>
+    <div className="text-center">
+      <h1>Hello from React!</h1>
+      <p>This is rendered by the React app.</p>
     </div>
   );
 }
