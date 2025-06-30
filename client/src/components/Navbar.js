f406e65b63f074904350df7fe69b259db45cd33d
Smartstudentcorner <Smartstudentcorner@gmail.com>
2025-06-30 20:47:11 +0530

    Add files via upload

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,1 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
 
+function Navbar() {
+  return (
+    <nav className="navbar navbar-expand navbar-dark bg-dark px-4">
+      <Link className="navbar-brand" to="/">MyApp</Link>
+      <div className="navbar-nav">
+        <Link className="nav-link" to="/">Home</Link>
+        <Link className="nav-link" to="/about">About</Link>
+        <Link className="nav-link" to="/contact">Contact</Link>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
