f0ea665b114a377dbfdb02587bf2fde5715b87ee
Smartstudentcorner <Smartstudentcorner@gmail.com>
2025-06-30 22:20:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express');
-const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'client/build')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/build/index.html'));
+// Root route
+app.get('/', (req, res) => {
+  res.send('Hello from Node.js backend!');
 });
 
+// Start the server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
