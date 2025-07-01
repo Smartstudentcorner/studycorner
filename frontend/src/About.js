4074d5b9f2151d264b7a1cb04443c392e0dba275
Smartstudentcorner <Smartstudentcorner@gmail.com>
2025-07-01 08:29:10 +0530

    Add files via upload

---
+++ frontend/src/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h1>About This Project</h1>
+      <p>This is a React + Node.js sample app styled with Bootstrap.</p>
+    </div>
+  );
+}
+
+export default About;
