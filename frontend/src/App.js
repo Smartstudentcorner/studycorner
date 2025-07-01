745999e40a55dd8e91131f6dc2152e32c6013206
Smartstudentcorner <Smartstudentcorner@gmail.com>
2025-07-01 08:28:31 +0530

    Create App.jst

---
+++ frontend/src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-3">
+          <Link className="navbar-brand" to="/">MyApp</Link>
+          <div className="collapse navbar-collapse">
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item">
+                <Link className="nav-link" to="/">Home</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/about">About</Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <div className="container mt-4">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
