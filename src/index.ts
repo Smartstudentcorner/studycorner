22c5bc9d67fcbe7b4b52d34321021c55ba976b48
Smartstudentcorner <Smartstudentcorner@gmail.com>
2025-07-02 22:04:27 +0530

    Add files via upload

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import express from 'express';
+import path from 'path';
+import indexRoutes from './routes/index';
+import apiRoutes from './routes/api';
+
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (_req, res) => {
-  res.send('Hello from TypeScript Node Web Service!');
-});
+app.use(express.static(path.join(__dirname, 'views')));
+app.use('/', indexRoutes);
+app.use('/api', apiRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
