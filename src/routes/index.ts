c71bd323f75d99d7ada22fa8bbcb562a515a162c
Smartstudentcorner <Smartstudentcorner@gmail.com>
2025-07-02 22:04:06 +0530

    Add files via upload

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,1 +1,17 @@
+import express from 'express';
+import path from 'path';
+const router = express.Router();
 
+router.get('/', (_req, res) => {
+  res.sendFile(path.join(__dirname, '../views/index.html'));
+});
+
+router.get('/about', (_req, res) => {
+  res.sendFile(path.join(__dirname, '../views/about.html'));
+});
+
+router.get('/contact', (_req, res) => {
+  res.sendFile(path.join(__dirname, '../views/contact.html'));
+});
+
+export default router;
