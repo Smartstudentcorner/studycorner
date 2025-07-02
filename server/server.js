780f10ab69a250890fec9ad78db2e56a151f06a0
Smartstudentcorner <Smartstudentcorner@gmail.com>
2025-07-02 08:27:26 +0530

    Add files via upload

--- server/server.js
+++ server/server.js
@@ -1,1 +1,20 @@
 
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// API
+app.get('/api/hello', (req, res) => {
+  res.json({ message: 'Hello from backend!' });
+});
+
+// Serve frontend
+app.use(express.static(path.join(__dirname, '../client/build')));
+app.get('*', (_req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
