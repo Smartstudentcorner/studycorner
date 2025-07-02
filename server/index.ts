eb323be13c0b3c320912eae01d8968a83c7a7b4a
Smartstudentcorner <Smartstudentcorner@gmail.com>
2025-07-02 22:18:44 +0530

    Add files via upload

--- server/index.ts
+++ server/index.ts
@@ -1,1 +1,14 @@
+import express from 'express';
+import path from 'path';
+import routes from './routes/index';
 
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, '../server/views')));
+
+app.use('/', routes);
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
