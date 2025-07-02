239b0c7821bd3d737a6870b575c4e3071712e4b1
Smartstudentcorner <Smartstudentcorner@gmail.com>
2025-07-02 21:52:23 +0530

    Add files via upload

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
+import express from 'express';
+const app = express();
+const port = process.env.PORT || 3000;
 
+app.get('/', (_req, res) => {
+  res.send('Hello from TypeScript Node Web Service!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
