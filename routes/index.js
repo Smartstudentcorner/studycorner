a6b93ddfd9f03f38cc9aac68acbb1eb6b7d10ca2
Smartstudentcorner <Smartstudentcorner@gmail.com>
2025-07-03 08:20:14 +0530

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,8 @@
+const express = require('express');
+const router = express.Router();
 
+router.get('/', (req, res) => res.render('pages/index', { title: 'Home' }));
+router.get('/about', (req, res) => res.render('pages/about', { title: 'About' }));
+router.get('/contact', (req, res) => res.render('pages/contact', { title: 'Contact' }));
+
+module.exports = router;
