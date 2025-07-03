cee5ae9766b7965c4903a20812b6f993c89a0791
Smartstudentcorner <Smartstudentcorner@gmail.com>
2025-07-03 08:31:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 const express = require('express');
-const app = express();
 const path = require('path');
-const indexRoutes = require('./routes/index');
+const app = express();
 
+// View Engine
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+// Static Files
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/', indexRoutes);
 
+// Routes
+app.get('/', (req, res) => res.render('pages/index'));
+app.get('/about', (req, res) => res.render('pages/about'));
+app.get('/contact', (req, res) => res.render('pages/contact'));
+
+// 404
 app.use((req, res) => res.status(404).render('404'));
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Study Corner running at http://localhost:${PORT}`));
