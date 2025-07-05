4f8246b4be434f61a489b9f475209c9de5c80da1
Smartstudentcorner <Smartstudentcorner@gmail.com>
2025-07-05 23:42:57 +0530

    Create variables.js

---
+++ variables.js
@@ -0,0 +1,14 @@
+// variables.js
+
+module.exports = {
+  siteTitle: 'The Study Corner',
+  creatorName: 'Your Name',
+  socialLinks: {
+    facebook: 'https://facebook.com',
+    twitter: 'https://twitter.com',
+    instagram: 'https://instagram.com',
+    youtube: 'https://youtube.com'
+  },
+  playStoreUrl: 'https://play.google.com/store/apps/details?id=com.example.app',
+  studyQuote: `"Education is the most powerful weapon which you can use to change the world." — Nelson Mandela`
+};
