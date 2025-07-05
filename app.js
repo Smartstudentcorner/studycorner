f70e9cd5125a088b49aaaea77f6df85ae3f97811
Smartstudentcorner <Smartstudentcorner@gmail.com>
2025-07-05 23:43:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,23 +1,36 @@
 const express = require('express');
-const path = require('path');
-const expressLayouts = require('express-ejs-layouts');
-
 const app = express();
-const PORT = process.env.PORT || 3000;
+const path = require('path');
 
-// EJS setup
+const variables = require('./variables'); // ⬅️ Load shared variables
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-app.use(expressLayouts);
-app.set('layout', 'layout/layout');
 
-// Static files
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
+
+// Inject variables into all views
+app.use((req, res, next) => {
+  res.locals.siteTitle = variables.siteTitle;
+  res.locals.creatorName = variables.creatorName;
+  res.locals.socialLinks = variables.socialLinks;
+  res.locals.playStoreUrl = variables.playStoreUrl;
+  res.locals.studyQuote = variables.studyQuote;
+  next();
+});
 
 // Routes
-app.get('/', (req, res) => res.render('pages/home', { title: 'Home - The Study Corner' }));
-app.get('/about', (req, res) => res.render('pages/about', { title: 'About - The Study Corner' }));
-app.get('/contact', (req, res) => res.render('pages/contact', { title: 'Contact - The Study Corner' }));
-app.get('/nolayout', (req, res) => res.render('pages/nolayout', { title: 'No Layout Page', layout: false }));
+app.get('/', (req, res) => {
+  res.render('pages/home', { title: 'Home' });
+});
 
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+app.get('/about', (req, res) => {
+  res.render('pages/about', { title: 'About' });
+});
+
+app.get('/contact', (req, res) => {
+  res.render('pages/contact', { title: 'Contact' });
+});
+
+// Start Server
+app.listen(3000, () => console.log('Server running on http://localhost:3000'));
